fbc8cba60a05be39f15e10aad3dcffd4f0581d6e
benconsult <bendayo38@gmail.com>
2024-05-01 18:45:54 +0100

    express file upload

--- starter/controllers/uploadsController.js
+++ starter/controllers/uploadsController.js
@@ -1,8 +1,15 @@
+const path = require('path')
 const {StatusCodes} = require('http-status-codes')
 
 const uploadProductImage = async (req,res) =>{
-  console.log(req);
-    res.send('upload product image');
+//   console.log(req);
+//   console.log(req.files);
+let prodductImage = req.files.image;
+
+const imagePath = path.join(__dirname,'../public/uploads/'+`${prodductImage.name}`)
+await prodductImage.mv(imagePath);
+ 
+    res.status(StatusCodes.OK).json({ image: {src:`/uploads/${prodductImage.name}`}})
 }
 
 module.exports = { uploadProductImage}
