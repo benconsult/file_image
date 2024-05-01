8ce8f781fd762759e60a33856efe829c12d9026e
benconsult <bendayo38@gmail.com>
2024-05-01 19:16:08 +0100

    express file upload - bug fixed

--- starter/controllers/productController.js
+++ starter/controllers/productController.js
@@ -4,14 +4,21 @@ const {StatusCodes} = require('http-status-codes')
 
 const createProduct = async (req,res) =>{
   console.log(req.body);
-  const product = await Product.create(req.body);
+  const product = await Product.create(req.body)
 
     res.status(StatusCodes.CREATED).json({product})
 };
 
 const getAllProducts = async (req,res)=>{
-    res.send('list of products');
+    const products = await Product.find({});
+    res.status(StatusCodes.OK).json({ products })
 
 }
 
 module.exports = { createProduct,getAllProducts}
+
+// const createJob = async (req, res) =>{
+//     req.body.createdBy =  req.user.userId
+//     const job = await Job.create(req.body)
+//     res.status(StatusCodes.CREATED).json({ job })
+// }
